422922d007ba3cb08c422084fc327642620d6413
andycasey <andycasey@gmail.com>
2013-04-18 20:28:14 +1000

    Added platform checks

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ import os
 import sys
 import fileinput
 import logging
+from platform import system as platform
 
 from shutil import copyfile, move
 from glob import glob
+
+# Check for platform first
+if platform not in ('Darwin', 'Linux'):
+    logging.critical("Platform '%s' not recognised!" % platform)
+    sys.exit()
 
 repository_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -32,22 +38,34 @@ for line in fileinput.input(hardcoded_moog_files, inplace=True):
     line = line.replace('$SRCDIR', srcdir)
     sys.stdout.write(line)
 
-os.system('cd moog;make -f Makefile.mac')
-os.system('cd moog;make -f Makefile.macsilent')
+# Which system are we on?
+if platform == 'Darwin':
+    run_make_files = ('Makefile.mac', 'Makefile.macsilent')
+
+elif platform == 'Linux':
+
+    is_64bits = sys.maxsize > 2**32
+
+    if is_64bits:
+        run_make_files = ('Makefile.rh64', 'Makefile.rh64silent')
+
+    else:
+        run_make_files = ('Makefile.rh', 'Makefile.rhsilent')
+
+
+# Run the appropriate make files
+for run_make_file in run_make_files:
+    os.system('cd moog;make -f %s' % run_make_file)
+
 
 # Cleanup files: Replace with original files
 [move(make_file + '.original', make_file) for make_file in make_files if os.path.exists(make_file + '.original')]
 [move(moog_file + '.original', moog_file) for moog_file in hardcoded_moog_files if os.path.exists(moog_file + '.original')]
 
 if os.getuid() == 0:
-    # We are sudo
+    # We are sudo; with great power comes great responsibility.
 
     # Copy the MOOG & MOOGSILENT to /usr/local/bin/
-    files_to_move = [
-        os.path.join(srcdir, 'MOOG'),
-        os.path.join(srcdir, 'MOOGSILENT')
-        ]
-
     copyfile(os.path.join(srcdir, 'MOOG'), '/usr/local/bin/MOOG')
     copyfile(os.path.join(srcdir, 'MOOGSILENT'), '/usr/local/bin/MOOGSILENT')
 
